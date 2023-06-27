d08c11cba0fa0e67dde0d4f69325ba638fdd41c6
MahirAlam <smmahiralamtohan@gmail.com>
2023-06-27 04:46:44 -0700

    first commit

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <section className="relative grid min-h-[89vh] place-content-center overflow-hidden">
-      ff
-    </section>
-  )
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,36 +2,19 @@
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 const colors = require("tailwindcss/colors")
-const { fontFamily } = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./page/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   future: {
     hoverOnlyWhenSupported: true,
   },
   theme: {
     extend: {
-      // https://vercel.com/design/color
       colors: {
-        "gray-1000": "#0D0D0D",
-        "gray-1001": "#161616",
-        "gray-1002": "#212121",
-        gray: {
-          ...colors.zinc,
-          one: "#a5a5a5",
-        },
-        brand: {
-          main: "#fff",
-        },
-      },
-      fontFamily: {
-        inter: ["Inter", ...fontFamily.sans],
-        grotesk: ["Space Grotesk", ...fontFamily.sans],
+        primary: colors.violet,
       },
     },
   },
